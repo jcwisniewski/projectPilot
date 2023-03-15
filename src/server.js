bca508bc8c5d6cb366e0b63c39d5355b8e21080f
Murilo <murilo.ramos@rspneus.com.br>
2023-03-15 10:15:16 -0300

    chore: commit test

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 require("dotenv").config();
 
 const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 
-const connectToDataBase = require("./database");
-
-connectToDataBase();
+app.use(routes);
 
 const port = 3333;
-
-// app.use(routes)
 
 app.listen(port, () => {
   console.log(`backend started at http://localhost:${port}`);
 });
+
+const connectToDataBase = require("./database");
+
+connectToDataBase();
