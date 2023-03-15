91dfab8d3850f31a248e8701dd3740edadc08fcd
Murilo <murilo.ramos@rspneus.com.br>
2023-03-15 10:18:08 -0300

    chore: commit test

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const routes = express.Router();
 
-routes.get("/", (req, res) => response.send("Hello World"));
+routes.get("/home", (req, res) => response.send("Hello World"));
 
 module.exports = routes;
