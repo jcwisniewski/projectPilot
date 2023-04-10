e999dd579206fa6b8caae2a61b65364a95c62205
jcwisniewski <julio.wisnieski@gmail.com>
2023-04-10 17:13:27 -0300

    chore: messageController test

--- src/controllers/MessageController.js
+++ src/controllers/MessageController.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const axios = require("axios");
 const { v4: uuid } = require("uuid");
 const Message = require("../models/Message");
 
@@ -15,6 +16,7 @@ module.exports = {
   },
 
   async SendMessage(request, response) {
+    const url = `${process.env.API_URL}/rest/sendMessage/text/?id=${process.env.API_TOKEN}`
     const { receiver, delay, message  } = request.body;
     if (receiver.length == 0 || message.length == 0) {
       return response.status(400).json({ error: "Receiver or message is null or empty" });
@@ -26,12 +28,16 @@ module.exports = {
       message
     });
 
-    try {
-    
-      await messages.save();
-      return response.status(201).json({status: response.status, message: messages});
+    try {    
+      const responseApi = await axios.post(url, request.body);
+      if(responseApi.data.success === true)
+      {
+        messages.save();        
+        return response.status(201).json({status: response.status, message: messages});
+      }        
     } catch (err) {
       return response.status(400).json({ error: err.message });
     }
   },
 }
+
