7e054c8249099d4304400545c1460c4ff2863dc0
jcwisniewski <julio.wisnieski@gmail.com>
2023-04-11 08:40:05 -0300

    chore: added cors to project

--- src/server.js
+++ src/server.js
@@ -2,18 +2,21 @@ require("dotenv").config();
 
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
-
-app.use(express.json());
-app.use(routes);
-
 const port = 3333;
 
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://project-pilot-pf7ysigxd-jcwisniewski.vercel.app"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(routes);
 app.listen(port, () => {
   console.log(`backend started at http://localhost:${port}`);
 });
-
 const connectToDataBase = require("./database");
-
 connectToDataBase();
